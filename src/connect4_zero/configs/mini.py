fd4ad43ecf03bbe656914aac23c5b445b2d5c000
Samuel <17341905+Zeta36@users.noreply.github.com>
2018-04-17 07:17:19 +0200

    Update mini.py

--- src/connect4_zero/configs/mini.py
+++ src/connect4_zero/configs/mini.py
@@ -1,6 +1,6 @@
 class EvaluateConfig:
     def __init__(self):
-        self.game_num = 10
+        self.game_num = 50
         self.replace_rate = 0.55
         self.play_config = PlayConfig()
         self.play_config.c_puct = 1
@@ -11,13 +11,13 @@ class EvaluateConfig:
 
 class PlayDataConfig:
     def __init__(self):
-        self.nb_game_in_file = 100
-        self.max_file_num = 10
+        self.nb_game_in_file = 50
+        self.max_file_num = 50
 
 
 class PlayConfig:
     def __init__(self):
-        self.simulation_num_per_move = 10
+        self.simulation_num_per_move = 100
         self.thinking_loop = 1
         self.logging_thinking = False
         self.c_puct = 5
@@ -33,16 +33,16 @@ class PlayConfig:
 
 class TrainerConfig:
     def __init__(self):
-        self.batch_size = 2048
+        self.batch_size = 1024
         self.epoch_to_checkpoint = 1
         self.start_total_steps = 0
-        self.save_model_steps = 1000
-        self.load_data_steps = 1000
+        self.save_model_steps = 100
+        self.load_data_steps = 100
 
 
 class ModelConfig:
-    cnn_filter_num = 16
-    cnn_filter_size = 3
-    res_layer_num = 1
+    cnn_filter_num = 64
+    cnn_filter_size = 4
+    res_layer_num = 3
     l2_reg = 1e-4
-    value_fc_size = 16
+    value_fc_size = 32
