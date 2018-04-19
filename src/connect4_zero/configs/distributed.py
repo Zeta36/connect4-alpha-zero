3b8ef2119e30cc2fa860263d328c2c790b663840
Samuel <17341905+Zeta36@users.noreply.github.com>
2018-04-19 17:53:55 +0200

    Update distributed.py

--- src/connect4_zero/configs/distributed.py
+++ src/connect4_zero/configs/distributed.py
@@ -3,7 +3,7 @@ class EvaluateConfig:
         self.game_num = 40  # 400
         self.replace_rate = 0.55
         self.play_config = PlayConfig()
-        self.play_config.simulation_num_per_move = 100
+        self.play_config.simulation_num_per_move = 50
         self.play_config.thinking_loop = 2
         self.play_config.c_puct = 1
         self.play_config.change_tau_turn = 0
@@ -19,7 +19,7 @@ class PlayDataConfig:
 
 class PlayConfig:
     def __init__(self):
-        self.simulation_num_per_move = 100
+        self.simulation_num_per_move = 50
         self.thinking_loop = 2
         self.logging_thinking = False
         self.c_puct = 1.5
